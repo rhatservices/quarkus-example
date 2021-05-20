cf4cbe0a3af2710df8b066a0964951820143e1c7
Toni Schmidbauer <toni@stderr.at>
2021-05-20 21:28:34 +0200

    fix indention

--- src/main/java/at/stderr/podResource.java
+++ src/main/java/at/stderr/podResource.java
@@ -13,19 +13,19 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 
 @Path("/pods")
 public class podResource {
-	private final KubernetesClient k8sClient;
-	
 
-	public podResource(KubernetesClient k8sClient) {
-		this.k8sClient = k8sClient;
-	}
-	
+    private final KubernetesClient k8sClient;
+
+    public podResource(KubernetesClient k8sClient) {
+	this.k8sClient = k8sClient;
+    }
+
     @GET
     @Path("{namespace}")
-    @Produces(MediaType.APPLICATION_JSON) 
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Pod> pods(@PathParam("namespace") String namespace) {
-    		List<Pod> pods = k8sClient.pods().inNamespace(namespace).list().getItems();
-    		//System.out.println(pods);
-    		return pods;
+	List<Pod> pods = k8sClient.pods().inNamespace(namespace).list().getItems();
+	//System.out.println(pods);
+	return pods;
     }
 }
